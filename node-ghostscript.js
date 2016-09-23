79f547a5b7878e09cb7a310d4e5bf6a7c88946c1
Sina Masnadi <sina.masnadi@gmail.com>
2016-09-23 14:25:29 -0400

    initialized npm

---
+++ node-ghostscript.js
@@ -0,0 +1,62 @@
+var exec = require('child_process').exec;
+var create = function () {
+    return new gs();
+};
+var gs = function () {
+    this.options = [];
+    this._input = null;
+};
+gs.prototype.batch = function () {
+    this.options.push('-dBATCH');
+    return this;
+};
+gs.prototype.device = function (device) {
+    device = device || 'jpeg';
+    this.options.push('-sDEVICE=' + device);
+    return this;
+};
+gs.prototype.exec = function (callback) {
+    var self = this;
+    if (!this._input) return callback("Please specify input");
+    var args = this.options.concat([this._input]).join(' ');
+    exec('gs ' + args, function (err, stdout, stderr) {
+        callback(err, stdout, stderr);
+    });
+};
+gs.prototype.input = function (file) {
+    this._input = file;
+    return this;
+};
+gs.prototype.nopause = function () {
+    this.options.push('-dNOPAUSE');
+    return this;
+};
+gs.prototype.output = function (file) {
+    file = file || '-';
+    this.options.push('-sOutputFile=' + file);
+    if (file === '-') return this.quiet();
+    return this;
+};
+gs.prototype.q = gs.prototype.quiet;
+gs.prototype.quiet = function () {
+    this.options.push('-dQUIET');
+    return this;
+};
+gs.prototype.firstPage = function (firstPage) {
+    this.options.push('-dFirstPage=' + firstPage);
+    return this;
+};
+gs.prototype.lastPage = function (lastPage) {
+    this.options.push('-dLastPage=' + lastPage);
+    return this;
+};
+gs.prototype.option = function (option) {
+    this.options.push(option);
+    return this;
+};
+gs.prototype.resolution = function (xres, yres) {
+    this.options.push('-r' + xres + (yres ? 'x' + yres : ''));
+    return this;
+};
+gs.prototype.r = gs.prototype.res = gs.prototype.resolution;
+module.exports = create; 
